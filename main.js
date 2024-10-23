c5f32e010a60459efb083fac191ee14ee2e47c69
PhatDiaz <phatdiaz2003@gmail.com>
2024-10-23 21:04:45 +0700

    Add feedback for user after calculate BMI

--- main.js
+++ main.js
@@ -7,7 +7,22 @@ const bmiForm = document.querySelector('#bmiForm');
 bmiForm.addEventListener('submit', function(e){
     e.preventDefault();
     let heightInMtr = height.value / 100;
-    result.innerHTML = 
-        (parseFloat(weight.value) / (heightInMtr*heightInMtr)).toFixed(3);
+    let bmi = (parseFloat(weight.value) / (heightInMtr * heightInMtr)).toFixed(3);
+    let feedback = "";
+
+    if(bmi < 18.5){
+        feedback = "You are underweight";
+    }
+    else if(bmi >= 18.5 && bmi < 24.9){
+        feedback = "You are normal weight";
+    }
+    else if(bmi >= 25 && bmi < 29.9){
+        feedback = "You are overweight";
+    }
+    else{
+        feedback = "You are obese";
+    }
+
+    result.innerHTML = `Your BMI is:&nbsp&nbsp${bmi}<br>(${feedback})`;
 })
 
