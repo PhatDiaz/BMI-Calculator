a75c8dc11d20a865424460ec47c73456d0485a5c
PhatDiaz <phatdiaz2003@gmail.com>
2024-10-24 12:15:37 +0700

    Update feedback clearly

--- main.js
+++ main.js
@@ -11,16 +11,16 @@ bmiForm.addEventListener('submit', function(e){
     let feedback = "";
 
     if(bmi < 18.5){
-        feedback = "You are underweight";
+        feedback = "You are underweight. Consider increasing your intake of nutrient-rich foods to reach a healthier weight.";
     }
     else if(bmi >= 18.5 && bmi < 24.9){
-        feedback = "You are normal weight";
+        feedback = "You have a normal weight. Keep maintaining a balanced diet and regular physical activity to stay healthy.";
     }
     else if(bmi >= 25 && bmi < 29.9){
-        feedback = "You are overweight";
+        feedback = "You are in the overweight range. It might be helpful to adjust your diet and increase physical activity to manage your weight.";
     }
     else{
-        feedback = "You are obese";
+        feedback = "You fall into the obese category. It's important to focus on a healthier lifestyle. Consider consulting a healthcare provider or nutritionist for personalized advice.";
     }
 
     result.innerHTML = `Your BMI is:&nbsp&nbsp${bmi}<br>(${feedback})`;
